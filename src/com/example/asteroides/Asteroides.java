d5db5cb8b74a963ea134a6669d0d915d2291a2c2
Gulivert <Aftsb88Github>
2013-06-12 18:31:10 +0200

    Tester

---
+++ src/com/example/asteroides/Asteroides.java
@@ -0,0 +1,70 @@
+package com.example.asteroides;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Asteroides extends Activity {
+
+	private Button bAcercaDe;
+	private Button bSalir;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		bAcercaDe =(Button) findViewById(R.id.button6);
+		bSalir = (Button) findViewById(R.id.button7);
+
+		bAcercaDe.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View view) {
+
+				lanzarAcercaDe(null);
+
+			}
+
+		});
+		bSalir.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View view) {
+				finish();
+			}
+		});
+	}
+
+
+	public void lanzarAcercaDe(View view){
+		Intent i = new Intent(this, AcercaDe.class);
+		startActivity(i);
+	}
+
+	public void salir(View view){
+		finish();
+	}
+
+	@Override 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true; /** true -> el menú ya está visible */
+	}
+
+	@Override 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			lanzarAcercaDe(null);
+			break;
+		}
+		return true; /** true -> consumimos el item, no se propaga*/
+	}
+}
